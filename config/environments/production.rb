c4b87a25d3f0e9c078f70280848116c5384638c5
bruno bornsztein <bruno.bornsztein@gmail.com>
2014-03-26 08:25:26 -0500

    precompile ce assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ DemoCommunityengineOrg::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( community_engine.js community_engine.css )
+  config.assets.precompile += %w(*.png *.gif *.jpg *.jpeg community_engine.js community_engine.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
