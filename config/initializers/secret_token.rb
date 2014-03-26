5a5c8dce23b3ea4d30a6cd0a30072e69c19016cf
bruno bornsztein <bruno.bornsztein@gmail.com>
2014-03-25 21:20:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoCommunityengineOrg::Application.config.secret_key_base = '5c8fce213e9099c6437178863883ecd4546ffd4e95b881eca8cb6785e020775c3325d8714665e21869311a59195ae852abdb7244fd165a475c543f1a58983b3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoCommunityengineOrg::Application.config.session_store :cookie_store, key: '_demo_communityengine_org_session'
